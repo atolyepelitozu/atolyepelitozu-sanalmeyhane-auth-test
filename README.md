f26d96e7a02ef3458a509b375aeb41efc5ca0cfb
talip4114 <92273678+atolyepelitozu@users.noreply.github.com>
2025-07-22 01:02:12 +0300

    Add files via upload
    İlk proje dosyaları yüklendi

---
+++ README.md
@@ -0,0 +1,2 @@
+# Sanal Meyhane Auth Test
+Bu proje Supabase ile SMS giriş testi için hazırlanmıştır.
---
+++ pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Hoş geldiniz: Sanal Meyhane Auth Test</h1>
+    </main>
+  );
+}
